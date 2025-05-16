23d18a7083ad0bfef54577cdc60b14428d9b1291
Zine-eddineBardoud <zineddinebardoud38@gmail.com>
2025-05-16 17:06:00 +0100

    Add Social Auth Form, Integrate NextAuth, OAuth with Google and GitHub

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,26 @@
-export default function Home() {
-    return <h1>Hello Next js</h1>;
-}
+import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
+import React from "react";
+
+const Home = async () => {
+    const session = await auth();
+
+    console.log(session);
+
+    return (
+        <>
+            <form
+                className="px-10 pt-[100px]"
+                action={async () => {
+                    "use server";
+                    await signOut({ redirectTo: ROUTES.SIGN_IN });
+                }}
+            >
+                <Button>Log Out</Button>
+            </form>
+        </>
+    );
+};
+
+export default Home;
